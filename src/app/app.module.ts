037fe523cba5640810be9152437dfe3b191d7611
MoverLee <leejaymover@gmail.com>
2017-11-06 22:42:26 +0800

    Function Or Bug: Step 1; Symption: N/A; BugId: N/A; Root Cause: N/A; Solution: N/A; Note: N/A;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { LayoutsComponent } from './layouts/layouts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    LayoutsComponent
   ],
   imports: [
     BrowserModule
